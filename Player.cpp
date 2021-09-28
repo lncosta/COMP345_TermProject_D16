418b71388cd2b9bda4ad339e8979a75caa89122f
Luiza Costa <luiza.n16@gmail.com>
2021-09-28 17:41:10 -0400

    Add files via upload

--- Player.cpp
+++ Player.cpp
@@ -1,81 +1,225 @@
-#include <iostream>
 #include "Player.h"
+#include <iostream>
+#include <vector>
+using namespace std;
 
-Player::Player(void){
-	playerID = 0;
+void OrdersList::addOrder(Order& other) {
+	listOfOrders.push_back(other);
+	count++;
+}
+OrdersList::OrdersList(void) {
+	count = 0;
+}
+
+int Player::playersCreated = 0;
+Player::Player(void)
+{
+	playersCreated++;
 	name = "Default";
-	list<Territory> empty;
-	list<Cards> empty2;
-	orders = NULL;
-	playersCreated++;
+	playerID = playersCreated;
+	orders = new OrdersList();
+	
+	
 }
 
-Player::Player(Player* other) {
-	playerID = 0;
-	name = "Default";
-	list<Territory> empty;
-	list<Cards> empty2;
-	orders = NULL;
+Player::Player(const Player &other)
+{
 	playersCreated++;
+	name = other.name;
+	playerID = other.playerID;
+	towned = other.towned;
+	cards = other.cards;
+	orders = other.orders;
+
+	
 }
+//Destructor:
 
-Player::Player(int playerID, string name) {
-	this->playerID = playerID;
-	this->name = name;
-	list<Territory> empty;
-	list<Cards> empty2;
-	orders = NULL;
-	playersCreated++;
-}
+//Player::~Player() { delete[] orders; }
 
-//Assignment Operators:
-
-void Player::setPlayerID(int playerID) {
+void Player::setPlayerID(int playerID)
+{
 	this->playerID = playerID;
 }
-void Player::setName(string name) {
+
+void Player::setName(string name)
+{
 	this->name = name;
 }
-void Player::setTOwned(list<Territory> towned) {
-	this->towned = towned;
+
+void Player::setTOwned(vector<Territory*> const &other)
+{
+	towned = other;
 }
-void Player::setCards(list<Cards> cards) {
+
+void Player::setCards(vector<Cards*>& cards)
+{
 	this->cards = cards;
 }
-void Player::setOrdersList(OrdersList* orders) {
-	this->orders = orders; 
+
+string Player::getName(void) {
+	return name;
 }
 
-//Stream Insertions:
-ostream& operator << (ostream& out, const Player& p) {
+int Player::getPlayerID(void) {
+	return playerID;
+}
+void Player::setOrdersList(OrdersList* orders)
+{
+	this->orders = orders;
+}
+void Player::addTerritory(Territory* ter) {
+	towned.push_back(ter);
+}
+void Player::addCard(Cards* other) {
+	cards.push_back(other);
+}
+vector<Territory*> Player::toDefend()
+{
+	Territory* a = new Territory();
+	Territory* b = new Territory();
+	Territory* c = new Territory();
+	a->name = "Canada";
+	b->name = "France";
+	c->name = "Japan";
+	vector<Territory*> defense;
+	defense.push_back(a);
+	defense.push_back(b);
+	defense.push_back(c);
+	return defense;
+}
 
-	out << "Player Name: " << p.name << endl;
-	out << "Player ID: " << p.playerID<< endl;
-	out << "Territories Owned: \n";
-	for(auto var: p.towned){
-		out << var << "\n";
+vector<Territory*> Player::toAttack()
+{
+	Territory* a = new Territory();
+	Territory* b = new Territory();
+	Territory* c = new Territory();
+	a->name = "Canada";
+	b->name = "France";
+	c->name = "Japan";
+	vector<Territory*> attack;
+	attack.push_back(a);
+	attack.push_back(b);
+	attack.push_back(c);
+	return attack;
+}
+void Player::printOrderList(void) {
+	cout << "----------------------------------" << endl;
+	cout << "The following commands are avaialable: " << endl;
+	cout << "DEPLOY: place some armies on one of the current player’s territories." << endl;
+	cout << "ADVANCE: move some armies from one of the current player’s territories(source) to an adjacent territory." << endl;
+	cout << "TARGET: If the target territory belongs to the current player, the armies are moved to the target.\nIf the target territory belongs to another player, an attack happens between the two territories." << endl;
+	cout << "BOMB: destroy half of the armies located on an opponent’s territory that is adjacent to one of the current player’s territories." << endl;
+	cout << "AIRLIFT: advance some armies from one of the current player’s territories to any another territory." << endl;
+	cout << "NEGOTIATE: prevent attacks between the current playerand another player until the end of the turn." << endl;
+	cout << "----------------------------------" << endl;
+}
+
+void Player::discoverOrderType(string x, Order &issued) {
+	string options[] = { "DEPLOY", "ADVANCE", "TARGET", "BOMB", "AIRLIFT", "NEGOTIATE" };
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (options[i].compare(x) == 0) {
+			issued.name = options[i];
+			return;
+		}
 	}
 
-	out << "Cards Owned: \n";
-	for (auto var : p.cards) {
-		out <<" Card \n";
+	issued.name = "UNKOWN ORDER";
+	return;
+}
+
+void Player::issueOrder()
+{
+	Order issued;
+	string x;
+	
+	printOrderList();
+	cout << "Please type out the order you would like to issue: " << endl;
+	cin >> x;
+	discoverOrderType(x, issued);
+	(*orders).addOrder(issued);
+	cout << "Order was issued: " << issued.name << endl;
+}
+
+ostream& operator<<(ostream& out, const Player& p)
+{
+	out << "Player: " << p.name << endl;
+	out << "ID: " << p.playerID << endl;
+	out << "Territories owned: " << endl;
+	for (Territory* i : p.towned) {
+		out << *i;
 	}
+	out << endl;
+	out << "Cards owned: " << endl;
+	for (Cards* i : p.cards) {
+		out << *i;
+	}
+	out << endl;
+	out << *(p.orders);
+	return out;
+}
 
-	out << "Current Orders: \n";
-	out << p.orders;
+istream& operator>>(istream& in, Player& p)
+{
+	in >> p.name;
+	return in;
+}
 
+ostream& operator<<(ostream& out, const Territory& p)
+{
+	out << p.name << endl;
+	return out;
+}
+
+istream& operator>>(istream& in, Territory& p)
+{
+	in >> p.name;
+	return in;
+}
+
+ostream& operator<<(ostream& out, const Cards& p)
+{
+	out << p.name << endl;
+	return out;
+}
+
+
+istream& operator>>(istream& in, Cards& p)
+{
+	in >> p.name;
+	return in;
+}
+
+
+
+ostream& operator<<(ostream& out, const Order& p)
+{
+	out << p.name << endl;
 	return out;
 
 }
-istream& operator >> (istream& in, Player& p) {
-	cout << "Enter Player Name: ";
-	in >> p.name;
 
-	return in;
+ostream& operator<<(ostream& out, const OrdersList& p)
+{
+	out << "The player has the following list of orders:" << endl;
+	for (Order i : p.listOfOrders) {
+		out << i;
+	}
+	return out;
 
 }
-//Methods:
-list<Territory> toDefend();
-list<Territory> toAttack();
-void issueOrder();
 
+istream& operator>>(istream& in, OrdersList& p)
+{
+	in >> p.count;
+	return in;
+}
+
+template <typename T>
+void printVector(vector<T> vec) {
+	for (T i : vec) {
+		cout << i << endl;
+	}
+}
--- Player.h
+++ Player.h
@@ -1,48 +1,87 @@
-#ifndef _PLAYER_H_
-#define _PLAYER_H_
-
-#include <list>
+#pragma once
+#include <vector>
 #include <iterator>
 #include <string>
 
-#include "Territory.h"
-#include "Cards.h"
-#include "OrdersList.h"
-	
+//#include "Territory.h"
+//#include "Cards.h"
+//#include "OrdersList.h"
+
 using namespace std;
 
-int playersCreated;
+
+class Territory {
+public:
+	string name;
+	string id;
+	friend ostream& operator << (ostream& out, const Territory& p);
+	friend istream& operator >> (istream& in, Territory& p);
+
+};
+
+class Cards {
+public:
+	string name;
+	friend ostream& operator << (ostream& out, const Cards& p);
+	friend istream& operator >> (istream& in, Cards& p);
+};
+class Order {
+public:
+	string name;
+};
+
+class OrdersList {
+public:
+	vector<Order> listOfOrders;
+	int count;
+	void addOrder(Order& other);
+	friend ostream& operator << (ostream& out, const OrdersList& p);
+	friend istream& operator >> (istream& in, OrdersList& p);
+	OrdersList(void);
+};
+
 
 class Player {
+static int playersCreated;
+private:
 	//Data members:
 	int playerID;
 	string name;
-	list<Territory> towned;
-	list<Cards> cards;
+	vector<Territory*> towned;
+	vector<Cards*> cards;
 	OrdersList* orders;
 
 public:
 	//Constructors:
 	Player(void);
-	Player(Player* other);
-	Player(int playerID, string name);
+	Player(const Player &other);
 
 	//Assignment Operators:
 
 	void setPlayerID(int playerID);
 	void setName(string name);
-	void setTOwned(list<Territory> towned);
-	void setCards(list<Cards> cards);
+	void setTOwned(vector<Territory*> const &other);
+	void setCards(vector<Cards*>& cards);
 	void setOrdersList(OrdersList* orders);
 
+	//Accessor methods:
+	string getName(void);
+	int getPlayerID(void);
+	
 	//Stream Insertions:
-	friend ostream& operator << (ostream& out, const Player &p);
+	friend ostream& operator << (ostream& out, const Player& p);
 	friend istream& operator >> (istream& in, Player& p);
+
+	//Destructors:
+	//~Player(void);
 	//Methods:
-	list<Territory> toDefend();
-	list<Territory> toAttack();
+	void printOrderList(void);
+	void discoverOrderType(string x, Order& issued);
+	vector<Territory*> toDefend();
+	vector<Territory*> toAttack();
 	void issueOrder();
+	void addTerritory(Territory* ter);
+	void addCard(Cards* other);
 
 };
 
-#endif 
