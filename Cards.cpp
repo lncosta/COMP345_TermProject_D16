244c4d00db5f327f932976c2d3a44a2944e99a9a
CSArie <87393395+CSArie@users.noreply.github.com>
2021-09-30 23:37:14 -0400

    Now with pointers!
    Draw() works, play() doesn't
    
    The next big hurdle is making play() work and then adding all the project requirements.

--- Cards.cpp
+++ Cards.cpp
@@ -1,33 +1,188 @@
-#include "Cards.h"
+/*
+play() still doesn't work but the rest seems to be working. I'm currently using the .cpp as a driver but 
+I'll split it once I'm done my implementation. 
 
-// Wil likely delete this line
-#include <iostream>
-using namespace std;
+missing some stream insertions (istream), copy constructors and assignment operators
 
-// Card::numberOfTypes = 5;
+*** The randomizer isn't random, this will get fixed later *** 
 
-Card::Card(int valueOfType) {
+*/
 
+#include "Cards.h";
+#include <iostream>;
+
+using std::cout;
+using std::endl;
+
+int numberOfTypes = 5;
+int numberOfPlayers = 5;
+
+void OrdersList::addOrder(Order* order) {
+	listOfOrders.push_back(order);
+}
+
+// Constructors
+Card::Card() {} 
+Card::Card(int type) { cardType = Type(type); }
+Card::Card(const Card& c) {cardType = c.cardType;}
+
+Hand::Hand() {}
+Hand::Hand(const Hand& h) { handOfCards = h.handOfCards; }
+
+Order::Order() { name = "default name"; }
+
+OrdersList::OrdersList() {}
+
+Player::Player(){ playerHand = new Hand(); playerOrders = new OrdersList();}
+
+Deck::Deck() {
+	int maxDeckSize = numberOfPlayers * numberOfTypes * 3;
+	for (int types = 0; types < numberOfTypes; types++) {
+		for (int cards = 0; cards < (3 * numberOfPlayers); cards++) {
+			Card c;
+			c.cardType = Type(types);
+			Card* pointer = new Card(c); // *TEST* adding these two lines and commenting out the other
+			deckOfCards.push_back(pointer);
+
+			//deckOfCards.push_back(new Card(c));
+		}
+	}
+}
+
+// Methods
+Card Deck::draw(Player& p) {
+
+	// Fisher-Yates shuffle algorithm
+	int size = deckOfCards.size();
+	for (int i = 0; i < (size - 1); i++) {
+		int j = i + rand() % (size - i);
+		std::swap(deckOfCards[i], deckOfCards[j]);
+	}
+
+	//vector2.push_back(vector1.pop_back());
+
+	// draw a card from the back of the deck and then remove it from the deck
+	Card* drawnCard = deckOfCards.back();
+	deckOfCards.pop_back();
+	// add the card to the player's hand
+
+	p.playerHand->handOfCards.push_back(drawnCard);
+	return *drawnCard;
 
 }
-void Card::play()
+//
+//void Card::play(Player& p, Deck& d) {
+//		Order ord;
+//		ord.name = Type(cardType);
+//		p.playerOrders.listOfOrders.push_back(ord);
+//		std::vector<Card*>::iterator it = std::find(p.playerHand.handOfCards.begin(), p.playerHand.handOfCards.end(), this);
+//		if (it != p.playerHand.handOfCards.end()) {
+//			//not needed	int index = std::distance(p.playerHand.handOfCards.begin(), it);
+//			 Card* cardPlayed = p.playerHand.handOfCards.at(it); 
+//			d.deckOfCards.push_back(cardPlayed);
+//			p.playerHand.handOfCards.erase(it);
+//			
+//		}
+//		
+//		else cout << "ERROR..." << endl;
+//	}
+
+// Ostreams
+
+ostream& operator<<(ostream& out, const Card& p)
 {
-    cout << "play() method called";
+	if (p.cardType == 0) {
+		out << "bomb" << endl;
+	}
+	else if (p.cardType == 1) {
+		out << "reinforcement" << endl;
+	}
+	else if (p.cardType == 2) {
+		out << "blockade" << endl;
+	}
+	else if (p.cardType == 3) {
+		out << "airlift" << endl;
+	}
+	else if (p.cardType == 4) {
+		out << "diplomacy" << endl;
+	}
+	else out << "Unknown card type" << endl;
 
-    if (cardType == BOMB) {
-        cout << "I just bombed you";
-    }
-    // Create a new order
-    // Remove the card from the hand and return it to the deck
+	return out;
 }
 
-// Deck
-Deck::Deck()
+ostream& operator << (ostream& out, const Hand& p) {
+	if (p.handOfCards.size() <= 0) {
+		cout << "The player doesn't have any cards" << endl;
+	}
+	else {
+		out << "The player currently has these cards: " << endl;
+		for (Card* c : p.handOfCards) {
+			out << *c;
+		}
+	}
+	return out;
+}
+
+ostream& operator << (ostream& out, const Deck& p) {
+	if (p.deckOfCards.size() <= 0) {
+		cout << "This deck is empty" << endl;
+	}
+	else {
+		out << "The deck currently contains these cards: " << endl;
+		for (Card* c : p.deckOfCards) {
+			out << *c;
+		}
+	}
+	return out;
+}
+
+ostream& operator<<(ostream& out, const Order& p)
 {
-    int numberOfPlayers = 4; // For testing purposes, will be removed
-        deckSize = numberOfPlayers * 5 * 3;
+	out << p.name << endl;
+	return out;
+
 }
 
-void Deck::printDeck(Deck myDeck) {
-    cout << "Test";
+ostream& operator<<(ostream& out, const OrdersList& p)
+{
+	if (p.listOfOrders.size() <= 0) {
+		cout << "The player doesn't have any orders" << endl;
+	}
+	else {
+		out << "The player has the following list of orders:" << endl;
+		for (Order* o : p.listOfOrders) {
+			out << *o;
+		}
+	}
+	return out;
+
+}
+
+ostream& operator << (ostream& out, const Player& p) {
+	out << *(p.playerHand) << endl;
+	out << *(p.playerOrders) << endl;
+
+	return out;
+}
+
+
+int main() {
+
+	Card c1(2);
+
+	cout << "Type of card c1 is: " << c1 << endl;
+	Deck d1;
+	Player p1;
+	cout << "Cards/Orders for p1 (should be empty): " << p1 << endl;
+	cout << "Size of the deck before cards are drawn: " << d1.deckOfCards.size() << endl;
+	cout << "The drawn cars if of type: " << d1.draw(p1) << endl;
+	cout << "The drawn cars if of type: " << d1.draw(p1) << endl;
+	cout << "The drawn cars if of type: " << d1.draw(p1) << endl;
+	cout << "The drawn cars if of type: " << d1.draw(p1) << endl;
+	cout << "The drawn cars if of type: " << d1.draw(p1) << endl;
+	cout << "Size of the deck after drawing 5 cards: " << d1.deckOfCards.size() << endl;
+	cout << "Size of the player's hand after drawing 5 cards: " << p1.playerHand->handOfCards.size() << endl;
+	cout << p1; 
+	cout << "-THE END" << endl; 
 }
--- Cards.h
+++ Cards.h
@@ -1,35 +1,131 @@
 #pragma once
-/*
-These are personal notes for when I'll inevitably forget how the hell c++ works
-*/
 #include <vector>
-using std::vector;
+#include <string>
+using std::string;
+using std::ostream;
+using std::istream;
+using std::vector; 
 
-class Deck
-{
-public: 
-    Deck();
-    vector<Card> deckOfCard; 
-    int  deckSize; // number of players * number of types * 3 
-    void printDeck(Deck myDeck);
-    void draw();
+enum Type { BOMB, REINFORCEMENT, BLOCKADE, AIRLIFT, DIPLOMACY };
+
+
+struct Order {
+	string name;
+	Order();
+
+	// Stream Insertion
+	friend ostream& operator << (ostream& out, const Order& p);
+	friend istream& operator >> (istream& in, Order& p);
 };
 
-class Hand
-{
-public: 
-    Hand();
-    vector<Card> handOfCard; 
+struct OrdersList {
+	vector<Order*> listOfOrders;
+	OrdersList();
+
+	// Stream Insertion
+	friend ostream& operator << (ostream& out, const OrdersList& p);
+	friend istream& operator >> (istream& in, OrdersList& p);
+
+	// Methods
+	void addOrder(Order* other);
 };
 
-class Card
-{
-public:
-    Card(int valueOfType);
-    Card();
-    int numberOfTypes;
-    enum Type { BOMB, REINFORCEMENT, BLOCKADE, AIRLIFT, DIPLOMACY };
-    Type cardType;
+struct Card {
 
-    void play();
+	// Data Members
+	Type cardType;
+
+	// Constructors
+	Card(); 
+	Card(int type);
+
+	// Copy constructor
+	Card(const Card& copy);
+
+	// Assignment Operators
+	void setCardtype(int cardType);
+
+	// Stream Insertions
+	friend ostream& operator << (ostream& out, const Card& p);
+	friend istream& operator >> (istream& in, Card& p);
+
+	// Methods
+	//void play(Player& p, Deck& d);
+
+	// Destructor
+	// ~Card();
+};
+
+struct Hand {
+
+	// Data Members
+	vector<Card*> handOfCards;
+
+	// Constructors
+	Hand();
+
+	// Copy constructor
+	Hand(const Hand& copy);
+
+	// Assignment Operators
+	void setHandOfCards(vector<Card*>& cards);
+
+	// Stream Insertions
+	friend ostream& operator << (ostream& out, const Hand& p);
+	friend istream& operator >> (istream& in, Hand& p);
+
+	// Destructor
+	// ~Hand();
+};
+
+struct Player {
+
+	// Data Members
+	Hand* playerHand;
+	OrdersList* playerOrders; 
+
+	// Constructors
+	Player();
+
+	// Copy constructor
+	Player(const Player& copy);
+
+	// Assignment Operators
+	void setPlayerHand(Hand* hand);
+	void setOrdersList(OrdersList* orders);
+
+	// Stream Insertions
+	friend ostream& operator << (ostream& out, const Player& p);
+	friend istream& operator >> (istream& in, Player& p);
+
+	// Destructor
+	// ~Player();
+};
+
+struct Deck {
+	// Data Members
+	vector<Card*> deckOfCards;
+	int maxDeckSize;
+
+	// Constructors
+	Deck();
+	
+
+	// Copy constructor
+	Deck(const Deck& copy);
+
+	// Assignment Operators
+	void setDeckOfCards(vector<Card*>& cards);
+	void setMaxDeckSize(int maxDeckSize);
+
+	// Stream Insertions
+	friend ostream& operator << (ostream& out, const Deck& p);
+	friend istream& operator >> (istream& in, Deck& p);
+
+	// Methods
+	Card draw(Player& p);
+	void printDeck();
+
+	// Destructor
+	// ~Deck();
 };
