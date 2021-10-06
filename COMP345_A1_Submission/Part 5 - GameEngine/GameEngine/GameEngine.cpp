1618cd1d0899649efe27fce611106803d21020c7
Yupei <73918497+YPHsu@users.noreply.github.com>
2021-10-06 11:48:33 -0400

    Update GameEngine.cpp

--- COMP345_A1_Submission/Part 5 - GameEngine/GameEngine/GameEngine.cpp
+++ COMP345_A1_Submission/Part 5 - GameEngine/GameEngine/GameEngine.cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 
+/*COMP 345 Section D - Assignment #1
+* Fall 2021
+* Due October 8th, 2021
+Written by
+Yupei Hsu 40139071
+Sarah-Noemie Laurin 40150861
+Arie Naccache 40099156
+Luiza Nogueira Costa 40124771
+Tomas Pereira 40128504
+*/
+
 GameEngine::GameEngine() 
 {
 	state = "Start";
