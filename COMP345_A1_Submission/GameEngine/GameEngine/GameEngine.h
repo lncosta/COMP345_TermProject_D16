89bbc32700c6c526bd9bdd35788b0fd868769407
Yupei <73918497+YPHsu@users.noreply.github.com>
2021-10-06 10:28:39 -0400

    Add files via upload

---
+++ COMP345_A1_Submission/GameEngine/GameEngine/GameEngine.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class GameEngine {
+	string state;
+
+public:
+	//Constructors:
+	GameEngine(void);
+	GameEngine(const GameEngine& other);
+
+	//Assignment Operators:
+	void setState(string newState);
+
+	//Accessor methods:
+	string getState();
+
+	//Stream Insertion:
+	friend ostream& operator << (ostream& out, const GameEngine& g);
+	friend istream& operator >> (istream& in, GameEngine& g);
+
+	//Destructors:
+	//~GameEngine(void);
+	
+	//Methods:
+	int menu(int i);
+
+
+	
+
+
+	
+};
