3157030703daa895dd70a7b72554bafa492d061c
Yupei <73918497+YPHsu@users.noreply.github.com>
2021-10-06 11:48:55 -0400

    Update GameEngineDriver.cpp

--- COMP345_A1_Submission/Part 5 - GameEngine/GameEngine/GameEngineDriver.cpp
+++ COMP345_A1_Submission/Part 5 - GameEngine/GameEngine/GameEngineDriver.cpp
@@ -1,5 +1,15 @@
 #include "GameEngine.h"
 
+/*COMP 345 Section D - Assignment #1
+* Fall 2021
+* Due October 8th, 2021
+Written by
+Yupei Hsu 40139071
+Sarah-Noemie Laurin 40150861
+Arie Naccache 40099156
+Luiza Nogueira Costa 40124771
+Tomas Pereira 40128504
+*/
 
 int main() {
 	int currentState = 0;
