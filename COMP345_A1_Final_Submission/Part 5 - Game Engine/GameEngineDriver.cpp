2c5bbe38a3c426a3932374daa37db6f48f9a0c2d
Yupei <73918497+YPHsu@users.noreply.github.com>
2021-10-07 14:18:13 -0400

    Add files via upload

---
+++ COMP345_A1_Final_Submission/Part 5 - Game Engine/GameEngineDriver.cpp
@@ -0,0 +1,17 @@
+#include "GameEngine.h"
+
+
+int main() {
+	int currentState = 0;
+	int newState = 0;
+
+	GameEngine ge;
+
+	do {
+		newState = ge.menu(currentState);
+		currentState = newState;
+
+	} while (currentState != -1);
+
+	return 0;
+}
