ffa5f6576e9b9362585597a82c531281ece7c9fe
Yupei <73918497+YPHsu@users.noreply.github.com>
2021-10-22 14:31:05 -0400

    Update GamePlayDriver.cpp

--- COMP345_A2_IntegratedFiles_VersionForA2DEV/GamePlayDriver.cpp
+++ COMP345_A2_IntegratedFiles_VersionForA2DEV/GamePlayDriver.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include "GameEngine.h"
 int main(void) {
+	
+	cout << "Welcome to Warzone! " << endl;
 	GameEngine* currentGame = new GameEngine(); 
 	currentGame->menu(0);
 	return 0;
