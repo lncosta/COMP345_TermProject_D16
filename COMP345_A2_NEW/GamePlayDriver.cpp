714534b58d47799a1f9e0a511e07dbbfaa6f54c8
Sarah Laurin <sarahno.laurin@gmail.com>
2021-11-05 17:18:05 -0400

    emptying the gamelog file

--- COMP345_A2_NEW/GamePlayDriver.cpp
+++ COMP345_A2_NEW/GamePlayDriver.cpp
@@ -3,7 +3,23 @@
 #include "GameEngine.h"
 
 int main(void) {
+	ofstream filewriting;
 	
+	filewriting.open("gamelog.txt");
+	
+	if (filewriting) {
+		//emptying file here
+		cout << "Emptying gamelog.txt for this game..." << endl;
+		cout << endl;
+		filewriting.open("gamelog.txt", std::ofstream::out | std::ofstream::trunc);
+		filewriting.close();
+	
+	}
+	else {
+		cout << "ERROR - File could not be opened!" << endl;
+		throw invalid_argument("ERROR - File could not be opened!");
+	}
+
 	cout << "Welcome to Warzone! " << endl;
 	GameEngine* currentGame = new GameEngine(); 
 	currentGame->menu(0);
